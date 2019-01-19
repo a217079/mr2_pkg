2e50e5bd9e92da2b039821b611037273f656ef9c
a217079 <a217079@ns.kogakuin.ac.jp>
2019-01-19 20:34:15 +0900

    歩いた

--- mr2_driver/src/simple_creep_gait.cpp
+++ mr2_driver/src/simple_creep_gait.cpp
@@ -3,12 +3,22 @@
 #include "ik_solver.h"
 #include "mr2_driver/LegsCommands.h"
 
-#define X 320
-#define Y 280
-#define Z -250
-#define STRIDE 150
-#define LIFT 150
+#define X 325
+#define X_SLIDE 65
+#define X_UP 400
+#define Y 325
+#define Z -230
+#define STRIDE 75
+#define LIFT 130
 #define ALPHA 0
+
+enum LegID
+{
+  FRONT_RIGHT,
+  FRONT_LEFT,
+  REAR_RIGHT,
+  REAR_LEFT,
+};
 
 void setTarget(int, Vector3d*);
 
@@ -20,8 +30,8 @@ int main(int argc, char **argv)
   ros::Publisher cmds_pub = n.advertise<mr2_driver::LegsCommands>("cmds", 10);
   mr2_driver::LegsCommands cmds;
   
-  ros::Rate loop_rate(5);
-  
+  ros::Rate loop_rate(2);
+
   IKSolver ik_fr(FRONT,RIGHT,REAL_MODEL);
   IKSolver ik_fl(FRONT,LEFT,REAL_MODEL);
   IKSolver ik_rr(REAR,RIGHT,REAL_MODEL);
@@ -32,6 +42,45 @@ int main(int argc, char **argv)
   Vector3d theta_rl;
   Vector3d target[4];
   int step = 0;
+
+  target[FRONT_RIGHT] << X+X_SLIDE, Y-STRIDE, Z;
+  target[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+  target[REAR_RIGHT] << X+X_SLIDE, -(Y-STRIDE), Z;
+  target[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
+  ik_fr.inverseKinematics(target[0]);
+  ik_fl.inverseKinematics(target[1]);
+  ik_rr.inverseKinematics(target[2]);
+  ik_rl.inverseKinematics(target[3]);
+  theta_fr = ik_fr.getDegree();
+  theta_fl = ik_fl.getDegree();
+  theta_rr = ik_rr.getDegree();
+  theta_rl = ik_rl.getDegree(); 
+
+  for(int i=0;i<3;i++)
+    cmds.deg[i] = theta_fr(i);
+  for(int i=0;i<3;i++)
+    cmds.deg[i+3] = theta_rr(i);
+  for(int i=0;i<3;i++)
+    cmds.deg[i+6] = theta_fl(i);
+  for(int i=0;i<3;i++)
+    cmds.deg[i+9] = theta_rl(i);
+  
+  cmds.deg[0] -= 10;
+  cmds.deg[1] -= -3;
+  cmds.deg[2] -= -4;
+  cmds.deg[3] -= -3;
+  cmds.deg[4] -= -11;
+  cmds.deg[5] -= 4.5;
+  cmds.deg[6] -= -9.5;
+  cmds.deg[7] -= 0;
+  cmds.deg[8] -= -12;
+  cmds.deg[9] -= 1;
+  cmds.deg[10] -= 8.5;
+  cmds.deg[11] -= -1;
+  
+  ros::Duration(5.0).sleep();
+  cmds_pub.publish(cmds);
+  ros::Duration(5.0).sleep();
 
   while(ros::ok())
   {
@@ -53,10 +102,20 @@ int main(int argc, char **argv)
       cmds.deg[i+6] = theta_fl(i);
     for(int i=0;i<3;i++)
       cmds.deg[i+9] = theta_rl(i);
-    cmds.deg[0] += 0;
-    cmds.deg[3] += 20;
-    cmds.deg[6] += 0;
-    cmds.deg[9] += 0;
+    
+    cmds.deg[0] -= 10;
+    cmds.deg[1] -= -3;
+    cmds.deg[2] -= -4;
+    cmds.deg[3] -= -3;
+    cmds.deg[4] -= -11;
+    cmds.deg[5] -= 4.5;
+    cmds.deg[6] -= -9.5;
+    cmds.deg[7] -= 0;
+    cmds.deg[8] -= -12;
+    cmds.deg[9] -= 1;
+    cmds.deg[10] -= 8.5;
+    cmds.deg[11] -= -1;
+    
     cmds_pub.publish(cmds);
 
     step = (step + 1) % 15;
@@ -67,107 +126,100 @@ int main(int argc, char **argv)
   return 0;
 }
 
-enum LegID
-{
-  FRONT_RIGHT,
-  FRONT_LEFT,
-  REAR_RIGHT,
-  REAR_LEFT,
-};
 
 void setTarget(int step, Vector3d* targetPtr)
 {
   switch(step)
   {
     case 0:
-      targetPtr[FRONT_RIGHT] << X, Y-(STRIDE+ALPHA), Z;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y-(STRIDE+ALPHA)), Z;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X+X_SLIDE, Y-(STRIDE+ALPHA), Z;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X+X_SLIDE, -(Y-(STRIDE+ALPHA)), Z;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 1:
-      targetPtr[FRONT_RIGHT] << X, Y-(STRIDE+ALPHA), Z+LIFT;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y-(STRIDE+ALPHA)), Z;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X_UP, Y-(STRIDE+ALPHA), Z+LIFT;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X+X_SLIDE, -(Y-(STRIDE+ALPHA)), Z;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 2:
-      targetPtr[FRONT_RIGHT] << X, Y+(STRIDE+ALPHA), Z+LIFT;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y-(STRIDE+ALPHA)), Z;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X_UP, Y+(STRIDE+ALPHA), Z+LIFT;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X+X_SLIDE, -(Y-(STRIDE+ALPHA)), Z;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 3:
-      targetPtr[FRONT_RIGHT] << X, Y+(STRIDE+ALPHA), Z;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y-(STRIDE+ALPHA)), Z;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X+X_SLIDE, Y+(STRIDE+ALPHA), Z;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X+X_SLIDE, -(Y-(STRIDE+ALPHA)), Z;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 4:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y-(STRIDE-ALPHA), Z;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y+(STRIDE-ALPHA)), Z;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X-X_SLIDE, Y-(STRIDE-ALPHA), Z;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X-X_SLIDE, -(Y+(STRIDE-ALPHA)), Z;
       break;
     case 5:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y-(STRIDE-ALPHA), Z;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y+(STRIDE-ALPHA)), Z+LIFT;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X-X_SLIDE, Y-(STRIDE-ALPHA), Z;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X_UP, -(Y+(STRIDE-ALPHA)), Z+LIFT;
       break;
     case 6:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y-(STRIDE-ALPHA), Z;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y-(STRIDE-ALPHA)), Z+LIFT;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X-X_SLIDE, Y-(STRIDE-ALPHA), Z;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X_UP, -(Y-(STRIDE-ALPHA)), Z+LIFT;
       break;
     case 7:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y-(STRIDE-ALPHA), Z;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y-(STRIDE-ALPHA)), Z;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X-X_SLIDE, Y-(STRIDE-ALPHA), Z;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X-X_SLIDE, -(Y-(STRIDE-ALPHA)), Z;
       break;
     case 8:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y-(STRIDE-ALPHA), Z+LIFT;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y-(STRIDE-ALPHA)), Z;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X_UP, Y-(STRIDE-ALPHA), Z+LIFT;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X-X_SLIDE, -(Y-(STRIDE-ALPHA)), Z;
       break;
     case 9:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y+(STRIDE-ALPHA), Z+LIFT;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y-(STRIDE-ALPHA)), Z;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X_UP, Y+(STRIDE-ALPHA), Z+LIFT;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X-X_SLIDE, -(Y-(STRIDE-ALPHA)), Z;
       break;
     case 10:
-      targetPtr[FRONT_RIGHT] << X, Y, Z;
-      targetPtr[FRONT_LEFT] << -X, Y+(STRIDE-ALPHA), Z;
-      targetPtr[REAR_RIGHT] << X, -Y, Z;
-      targetPtr[REAR_LEFT] << -X, -(Y-(STRIDE-ALPHA)), Z;
+      targetPtr[FRONT_RIGHT] << X-X_SLIDE, Y, Z;
+      targetPtr[FRONT_LEFT] << -X-X_SLIDE, Y+(STRIDE-ALPHA), Z;
+      targetPtr[REAR_RIGHT] << X-X_SLIDE, -Y, Z;
+      targetPtr[REAR_LEFT] << -X-X_SLIDE, -(Y-(STRIDE-ALPHA)), Z;
       break;
     case 11:
-      targetPtr[FRONT_RIGHT] << X, Y-(STRIDE+ALPHA), Z;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y+(STRIDE+ALPHA)), Z;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X+X_SLIDE, Y-(STRIDE+ALPHA), Z;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X+X_SLIDE, -(Y+(STRIDE+ALPHA)), Z;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 12:
-      targetPtr[FRONT_RIGHT] << X, Y-(STRIDE+ALPHA), Z;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y+(STRIDE+ALPHA)), Z+LIFT;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X+X_SLIDE, Y-(STRIDE+ALPHA), Z;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X_UP, -(Y+(STRIDE+ALPHA)), Z+LIFT;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 13:
-      targetPtr[FRONT_RIGHT] << X, Y-(STRIDE+ALPHA), Z;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y-(STRIDE+ALPHA)), Z+LIFT;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X+X_SLIDE, Y-(STRIDE+ALPHA), Z;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X_UP, -(Y-(STRIDE+ALPHA)), Z+LIFT;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
     case 14:
-      targetPtr[FRONT_RIGHT] << X, Y-(STRIDE+ALPHA), Z;
-      targetPtr[FRONT_LEFT] << -X, Y, Z;
-      targetPtr[REAR_RIGHT] << X, -(Y-(STRIDE+ALPHA)), Z;
-      targetPtr[REAR_LEFT] << -X, -Y, Z;
+      targetPtr[FRONT_RIGHT] << X+X_SLIDE, Y-(STRIDE+ALPHA), Z;
+      targetPtr[FRONT_LEFT] << -X+X_SLIDE, Y, Z;
+      targetPtr[REAR_RIGHT] << X+X_SLIDE, -(Y-(STRIDE+ALPHA)), Z;
+      targetPtr[REAR_LEFT] << -X+X_SLIDE, -Y, Z;
       break;
   }
 }
